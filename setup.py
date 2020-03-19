ead69eededf3d1e416505a2f4602fac1b3218946
bganglia <bganglia892@gmail.com>
2020-03-19 10:58:35 -0400

    add classifiers

--- setup.py
+++ setup.py
@@ -2,19 +2,23 @@ from setuptools import setup
 import os
 
 readme_path = os.path.join(os.path.dirname(__file__),"README.md")
-with open(readme_path) as handle:
+with open(readme_path, "r") as handle:
     README = handle.read()
 
 setup(
     name="unpaywall-python",
     version="0.0.1",
     description="Programmatically access open access (OA) articles",
-    long_description="README",
+    long_description=README,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/bganglia/unpaywall-python",
     author="bganglia",
     author_email="bganglia892@gmail.com",
     license="MIT",
-    classifiers=[],
-    packages=[]
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages()
 )
