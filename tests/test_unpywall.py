0925415d285efcb7cdea7e40acf9c30e02c63ab2
bganglia <bganglia892@gmail.com>
2020-04-17 20:33:01 -0400

    use TestUnpywallCache class instead

--- tests/test_unpywall.py
+++ tests/test_unpywall.py
@@ -83,16 +83,17 @@ class TestUnpywall:
 
 test_dir = os.path.abspath(os.path.dirname(__file__))
 
-@pytest.fixture
-def example_cache():
-    copyfile(os.path.join(test_dir, "example_cache"),
-             os.path.join(test_dir, "test_cache"))
-    cache = UnpywallCache(os.path.join(test_dir, "test_cache"))
-    assert cache.content != {}
-    assert cache.access_times != {}
-    return cache
+class TestUnpywallCache:
+    @pytest.fixture
+    def example_cache(self):
+        copyfile(os.path.join(test_dir, "example_cache"),
+                 os.path.join(test_dir, "test_cache"))
+        cache = UnpywallCache(os.path.join(test_dir, "test_cache"))
+        assert cache.content != {}
+        assert cache.access_times != {}
+        return cache
 
-def test_reset_cache(example_cache):
-    example_cache.reset_cache()
-    assert example_cache.content == {}
-    assert example_cache.access_times == {}
+    def test_reset_cache(self, example_cache):
+        example_cache.reset_cache()
+        assert example_cache.content == {}
+        assert example_cache.access_times == {}
