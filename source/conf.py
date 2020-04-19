99bdeb68cc08aaa56a35389331c5a3f02b6d3112
bganglia <bganglia892@gmail.com>
2020-04-19 14:58:59 -0400

    add parent directory to path

--- source/conf.py
+++ source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -23,7 +23,6 @@ author = 'Nick Haupka, bganglia'
 
 # The full version, including alpha/beta/rc tags
 release = 'MIT'
-
 
 # -- General configuration ---------------------------------------------------
 
