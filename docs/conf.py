82285aeb050c64fc635a44779c46df274fe71a5e
bganglia <bganglia892@gmail.com>
2020-04-20 15:31:03 -0400

    add napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,8 @@ release = 'MIT'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc"
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
